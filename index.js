5b054a85dcd4f176300b7f1266f8a097f9c961b1
Shivam Joshi <joshishivam108@gmail.com>
2024-04-03 10:31:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// const http = require("http");
+
+// http
+//   .createServer((req, resp) => {
+//     resp.write("<h1>Hello this is shivam Joshi</h1>");
+//     resp.end();
+//   })
+//   .listen(4501);
